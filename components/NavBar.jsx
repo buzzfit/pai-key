489c776f8a8bacff22f7566c546e7877115af5f4
buzzfit <168493535+buzzfit@users.noreply.github.com>
2025-05-10 14:21:01 -0400

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,10 +5,10 @@ export default function NavBar() {
       <div className="max-w-6xl mx-auto p-4 flex items-center">
         <a href="/" className="text-xl font-bold">PAI Key</a>
         <div className="flex ml-auto">
-          <a href="/" className="mx-4 hover:underline">Home</a>
-          <a href="#features" className="mx-4 hover:underline">Features</a>
-          <a href="https://github.com/buzzfit/pai-key" className="mx-4 hover:underline">GitHub</a>
-          <a href="https://app.pai-key.org" className="mx-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Get Started</a>
+          <a href="/" className="hover:underline">Home</a>
+          <a href="#features" className="hover:underline">Features</a>
+          <a href="https://github.com/buzzfit/pai-key" className="hover:underline">GitHub</a>
+          <a href="https://app.pai-key.org" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Get Started</a>
         </div>
       </div>
     </nav>
