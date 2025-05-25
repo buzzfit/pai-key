f017f985591034b33178f9e8d2c71831709cbccf
buzzfit <168493535+buzzfit@users.noreply.github.com>
2025-05-24 20:53:09 -0400

    fix: add 'use client' directive to Home page and modals

--- app/page.jsx
+++ app/page.jsx
@@ -5,70 +5,60 @@ import { useState, useEffect } from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import JobTemplateForm from '../components/JobTemplateForm';
-+ import EmailSignupForm from '../components/EmailSignupForm';
+import EmailSignupForm from '../components/EmailSignupForm';
 
 export default function Home() {
-- const [showForm, setShowForm] = useState(false);
-+ const [showSignup, setShowSignup] = useState(false);
-+ const [showForm, setShowForm] = useState(false);
-+ const [signedUp, setSignedUp] = useState(false);
+  const [signedUp, setSignedUp] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-  // On mount, check localStorage
+  // On mount, check if user already signed up
   useEffect(() => {
--   // nothing
-+   if (localStorage.getItem('paiKeySignedUp') === 'true') {
-+     setSignedUp(true);
-+   }
+    if (localStorage.getItem('paiKeySignedUp') === 'true') {
+      setSignedUp(true);
+    }
   }, []);
 
-  // handle email signup
-+ const handleSignUp = email => {
-+   console.log('Signed up:', email);
-+   setSignedUp(true);
-+   setShowSignup(false);
-+   // now open job template
-+   setShowForm(true);
-+ };
+  // After successful signup
+  const handleSignUp = (email) => {
+    console.log('Signed up:', email);
+    setSignedUp(true);
+    setShowSignup(false);
+    setShowForm(true);
+  };
 
-  // open the job template or signup
-- const handleOpenForm = () => setShowForm(true);
-+ const handleOpenForm = () => {
-+   if (signedUp) {
-+     setShowForm(true);
-+   } else {
-+     setShowSignup(true);
-+   }
-+ };
-
-  const handleCloseForm = () => setShowForm(false);
-+ const handleCloseSignup = () => setShowSignup(false);
-
-  const handleFormSubmit = data => {
-    console.log('Job Template Data:', data);
-    setShowForm(false);
+  // Entry point for Get Started button
+  const handleOpenForm = () => {
+    if (signedUp) {
+      setShowForm(true);
+    } else {
+      setShowSignup(true);
+    }
   };
 
   return (
     <>
-+     {/* Signup Modal */}
-+     {showSignup && (
-+       <EmailSignupForm
-+         onSignUp={handleSignUp}
-+         onClose={handleCloseSignup}
-+       />
-+     )}
+      {/* Email Signup Modal */}
+      {showSignup && (
+        <EmailSignupForm
+          onSignUp={handleSignUp}
+          onClose={() => setShowSignup(false)}
+        />
+      )}
 
       {/* Job Template Modal */}
       {showForm && (
         <JobTemplateForm
-          onSubmit={handleFormSubmit}
-          onClose={handleCloseForm}
+          onSubmit={(data) => {
+            console.log('Job Template Data:', data);
+            setShowForm(false);
+          }}
+          onClose={() => setShowForm(false)}
         />
       )}
 
-      {/* Main content */}
--     <Hero onGetStarted={handleOpenForm} />
-+     <Hero onGetStarted={handleOpenForm} />
+      {/* Main Page */}
+      <Hero onGetStarted={handleOpenForm} />
       <Features />
     </>
   );
