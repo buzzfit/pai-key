9ea7ea253c50b4ce6771528b05b5bb43a83733d9
buzzfit <168493535+buzzfit@users.noreply.github.com>
2025-06-28 18:49:44 -0400

    add /vendors page that opens VendorDockForm

---
+++ app/vendors/page.jsx
@@ -0,0 +1,29 @@
+'use client';
+import { useState } from 'react';
+import VendorDockForm from '../../components/VendorDockForm';
+
+export default function VendorsPage() {
+  const [open, setOpen] = useState(true);
+  return (
+    <>
+      {open && (
+        <VendorDockForm
+          onSubmit={data => {
+            console.log('TODO: POST /api/vendors', data);
+            setOpen(false);
+          }}
+          onClose={() => setOpen(false)}
+          onConnectWallet={async () => {
+            alert('TODO: wire Xumm connect');
+            return '';
+          }}
+        />
+      )}
+      {!open && (
+        <div className=\"flex h-screen items-center justify-center text-xl\">
+          Thanks for registering your agent!
+        </div>
+      )}
+    </>
+  );
+}
